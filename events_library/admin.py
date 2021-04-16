0a41e6954edda926bb37a5584076c85562172cfb
Daniel Gonzalez Fernandez <eldano1995@gmail.com>
2021-04-16 18:54:08 -0400

    feat: Create resend_failed_events action in EventLogAdmin

--- events_library/admin.py
+++ events_library/admin.py
@@ -1,9 +1,10 @@
 import typing
 
 from django.contrib import admin
-from django.db.models import Model
+from django.db.models import Model, QuerySet, query
 from django.http.request import HttpRequest
 
+from .core import EventApi
 from .domain import EventLog, HandlerLog
 
 
@@ -33,11 +34,44 @@ class EventLogAdmin(InmutableAdminModel):
         "target_service",
     ]
     list_display = [
-        'id', 'event_type',
-        'target_service',
-        'was_success', 'created_at',
+        'id', 'event_type', 'target_service',
+        "retry_number", 'was_success', 'created_at',
     ]
     ordering = ["-created_at"]
+    actions = ["resend_failed_events"]
+
+    def resend_failed_events(self, request: HttpRequest, queryset: QuerySet[EventLog]) -> None:
+        """Resends failed EventLogs selected by the user"""
+        api = EventApi()
+        fail_count = 0
+        total = queryset.count()
+
+        for event_log in queryset.filter(was_success=False):
+            event_request_summary = api.send_event_request(
+                event_log.target_service,
+                event_log.event_type,
+                event_log.payload,
+            )
+
+            event_log.was_success = event_request_summary["was_success"]
+            if not event_log.was_success:
+                fail_count += 1
+                # Update last error_message only in this case (in case
+                # we want to see more common bugs in our service system)
+                event_log.error_message = event_request_summary["error_message"]
+
+            # Count all retries since creation of the EventLog
+            event_log.retry_number += event_request_summary["retry_number"]
+            event_log.save()
+
+        self.message_user(
+            request,
+            f"Finished resending events: {fail_count} (out of {total}) failed"
+        )
+
+    resend_failed_events.short_description = (
+        "Mark only failed events: successful ones will be resended"
+    )
 
 
 @admin.register(HandlerLog)
